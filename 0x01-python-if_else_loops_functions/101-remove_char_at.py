1887909f70843479fb47ef2b9667877b9eb74f8a
Vpsalmuel <vpsalmuel@gmail.com>
2022-05-31 16:03:45 +0000

    if_elseif_loops:
    "
    
    git push

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+
+    c = ""
+
+    for i in range(len(str)):
+
+        if i != n:
+
+            c = c + str[i]
+
+    return (c)
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+if_else_and loops in python
