ca59f2082743ab17de63fd2a832c81ccfef1ea11
Vpsalmuel <vpsalmuel@gmail.com>
2022-05-31 16:27:07 +0000

    files added

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,11 +2,10 @@
 
 def islower(c):
 
-    num = ord(c)
-
-    if num >= 97 and num <= 122:
+    if ord(c) >= 97 and ord(c) <= 122:
 
         return (True)
+
     else:
 
-    return (False)
+        return (False)
